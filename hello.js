79bdc5d8158562ef09a65ad449a7663a0e9c28fe
sabbirsun <sabbirsun1920@gmail.com>
2024-01-08 22:28:16 +0600

    function

--- hello.js
+++ hello.js
@@ -31,7 +31,25 @@ num1 = parseFloat(num1);//parseInt
 num1 = "" + num1;//int to string
 console.log(num1 + num2)
 
+var num3 = 80;
+var num4 = 60;
+//console.log(num3 + num4);//+ - * /
+//% bhagsesh
+
+var total = num3 % num4;
+console.log(total);
+
+var num5 = -80;
+//var num6 = 60;
 
 
+//var sum = Math.abs(num5);//negitive value ke plus kore
+var totalMembers = 10;
+var voter = 5.5;
 
-
+//var sum = Math.round(voter);//kind of vote 0.5 er upore 1 0.5 er niche 0
+//var sum = Math.ceil(voter);//0 theke boro holei 1
+//var sum = Math.floor(voter);//0.9999 thakleo niche namay dibe 0 hbe
+//console.log(sum);
+var lotary = Math.random() * 100;//random number
+console.log(lotary);
