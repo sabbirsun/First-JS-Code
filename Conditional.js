6a057bb2eec1baba700662d537e1f28838606067
sabbirsun <sabbirsun1920@gmail.com>
2024-01-10 01:30:43 +0600

    date

--- Conditional.js
+++ Conditional.js
@@ -12,7 +12,7 @@ if(a < b){
 
 // switch case Statement
 
-var sum = 100;
+/*var sum = 100;
 
 switch(sum){
     case 10:
@@ -25,6 +25,18 @@ switch(sum){
         console.log("Now, I am in 10");
         break;
     default:
-        console.log("I don't msg");
-        
-}
+        console.log("I don't msg");    
+}*/
+
+// Real time date
+
+var myDate = new Date();
+
+console.log(myDate.toTimeString());//Bangladesh Time
+console.log(myDate.toLocaleTimeString());// AM PM show
+console.log(myDate.toLocaleDateString());// Date show
+console.log(myDate.getMonth());// Month
+console.log(myDate.getDate());// Today Date
+console.log(myDate.getMinutes());
+console.log(myDate.getSeconds());
+console.log(myDate.getMilliseconds());
