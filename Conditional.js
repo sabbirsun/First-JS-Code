118fa617beb82a84d8b5968344577c6afc610e76
sabbirsun <sabbirsun1920@gmail.com>
2024-01-10 01:14:58 +0600

    switch case

--- Conditional.js
+++ Conditional.js
@@ -1,4 +1,5 @@
-var a = 10;
+// if else Statement
+/*var a = 10;
 var b = 10;
 
 if(a < b){
@@ -7,4 +8,23 @@ if(a < b){
     console.log("b boro");
 }else{
     console.log("They both are same");
+}*/
+
+// switch case Statement
+
+var sum = 100;
+
+switch(sum){
+    case 10:
+        console.log("Now, I am in 10");
+        break;
+    case 5:
+        console.log("Now, I am in 10");
+        break;
+    case 1:
+        console.log("Now, I am in 10");
+        break;
+    default:
+        console.log("I don't msg");
+        
 }
