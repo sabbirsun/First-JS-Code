988a45ba43d09f2942f71619a0828636af5e548a
sabbirsun <sabbirsun1920@gmail.com>
2024-01-11 00:28:28 +0600

    Object

--- Function.js
+++ Function.js
@@ -1,9 +1,27 @@
 //!important
-function Fun(num){
+/*function Fun(num){
     var total = num * 10;
     return total
     //console.log(total);
 }
 var a = Fun(10);
 var b =Fun(20);
-console.log(a + b);
+console.log(a + b);*/
+
+// Object
+
+function myInfo(YourName, Village, PhoneNumber){
+    this.YourName = YourName;
+    this.Village = Village;
+    this.PhoneNumber = PhoneNumber;
+    this.showMe = function(){
+        console.log(this.YourName)
+        console.log(this.Village)
+        console.log(this.PhoneNumber)
+    }
+}
+    var newObj = new myInfo("Sun", "Sirajganj", 017);
+    var newObj2 = new myInfo("Sun", "Sirajganj", 017);
+    newObj.showMe();
+    console.log("break");
+    newObj2.showMe();
