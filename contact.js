f88ceb75372fdca5bfd17edfaee4d30159c75927
sabbirsun <sabbirsun1920@gmail.com>
2024-01-11 12:43:40 +0600

    scope

--- contact.js
+++ contact.js
@@ -1,3 +1,19 @@
+/*var let const
 var num = 0;// Same value
 let num1 = 0;// Different value
-const num2 = 0;// Different value
+const num2 = 0;// Different value*/
+
+// Scope
+
+/*var fdName = "Sabbir Sun"; // je kono jaiga theke access korte parbo....Global Scope
+console.log(fdName);
+function me(){
+    var num = "Sabbir Sun";
+    console.log(num);
+}
+me()
+
+{
+    let tumi = "tomar name ki";
+    console.log(tumi);
+}*/
